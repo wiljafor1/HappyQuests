22ba1f61092dcb4087788e16387f9cb8405a3898
DevNatan <itroken99@gmail.com>
2019-01-16 00:45:33 -0200

    Push TaskManager and plugin.yml.

---
+++ src/main/kotlin/me/devnatan/happymc/quests/manager/TaskManager.kt
@@ -0,0 +1,13 @@
+package me.devnatan.happymc.quests.manager
+
+import me.devnatan.happymc.quests.HappyQuests
+
+class TaskManager(val plugin: HappyQuests) {
+
+    init {
+
+    }
+
+    fun interrupt() {}
+
+}
